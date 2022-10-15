8e41c2178bc4e5d0c866221bd62021e949583d08
Ahmad <ahmedmageed19@gmail.com>
2022-10-15 02:30:45 +0200

    feat(server): authorization, checks and reports routing

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 require("dotenv").config();
+const usersRouter = require("./Routes/users");
+const urlChecksRouter = require("./Routes/urlChecks");
+const reportsRouter = require("./Routes/reports");
+
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./Config/config.db");
-const usersRouter = require("./Routes/users");
 const errorHandler = require("./Middlewares/errorHandler");
+const authorize = require("./Middlewares/authorize");
+
 const PORT = process.env.port || 8000;
 const app = express();
 
 
 // Routes & Middlewares
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
-
+app.use("/api/v1/checks/", authorize, urlChecksRouter);
+app.use("/api/v1/reports/", authorize, reportsRouter);
 
 app.use(errorHandler);
 // Server
